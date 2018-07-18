1ae89f71cd6cd2090806b2ae3aba0036d4f796b5
meihuo_fyn@163.com <meihuo_fyn@163.com>
2018-07-18 19:15:27 +0800

    更新组件库和文档库

--- source/index/controllers/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var module = angular.module('app.controllers');
-
-module.controller('index', function($scope, $element, $http) {
-    //从menu.js中获取顶部导航数据
-    $scope.menus = nav;
-
-})
